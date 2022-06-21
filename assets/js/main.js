77278583046eca1c6ff9ad90d105ae1285802c56
mito01100010 <dimitar.pashkulev@gmail.com>
2022-06-21 19:48:07 +0300

    Fix nav-trigger

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 const body = document.getElementsByTagName('body')[0];
 const navTrigger = document.querySelector('.nav-trigger');
-const heroTrigger = document.querySelector('.hero-trigger');
 const hamMenu = document.querySelector('.js-menu');
 
-navTrigger.addEventListener('click', () => {
+navTrigger.addEventListener('click', (e) => {
+    e.preventDefault();
     navTrigger.classList.toggle('show');
     hamMenu.classList.toggle('show');
     body.classList.toggle('hidden-overflow');
